cb940ee481eab8515c4535ebf040672db2e6ccf6
leota0608 <xichen.tao1@rbe.sk.ca>
2025-10-30 13:31:51 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+class Room:
+    def __init__(self, coordinate, description):#required
+        #attributes
+        self.coordinate = coordinate
+        self.description = description
+        self.chest = False
+        
+    def __str__(self):#print the object print(ftc)
+        return f"This room is at ({self.coordinate[0]}, {self.coordinate[1]})\n{self.description}"
+    
+    def place_chest(self):
+        self.chest = True
+ 
+        
+
+house = []
+for i in range(4):
+    a = []
+    for j in range(4):
+        a.append(Room([i,j], "Nothing here..."))
+    house.append(a)
+house[0][0].description = "Starting position"
+house[3][3].description = "Exit"
+
+num_chest = 5
+available_spot = []
+for i in range(4):
+    for j in range(4):
+        if i == 0 and j == 0 or i == 3 and j == 3:
+            continue
+        available_spot.append([i, j])
+for i in range(num_chest):
+    a = random.randint(0, len(available_spot)-1)
+    house[available_spot[a][0]][available_spot[a][1]].place_chest()
+    
