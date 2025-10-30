5d9b5da814a76ac3d020e45fd93dbec9086176fb
leota0608 <xichen.tao1@rbe.sk.ca>
2025-10-30 13:58:44 -0600

    .

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # treasure_chest_assignment
-
+<pre>
+Story line:
+    You are in a forest, there are 16 trees placed into a 4*4 grid,
+    there are treasure chests burried inside some of the trees, and 
+    keys can also be found in them. Use keys to open chests and escape
+    successfully! Remember what cames with treasure is danger...
+</pre>
 This code does the following:
 - Creates a room `class`
 - Creates a 2d `list` containing 4*4 = 16 room classes
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import random
 class Room:
-    def __init__(self, coordinate, description):#required
+    def __init__(self, coordinate, description):
         #attributes
         self.coordinate = coordinate
         self.description = description
         self.chest = False
         
-    def __str__(self):#print the object print(ftc)
+    def __str__(self):
         return f"This room is at ({self.coordinate[0]}, {self.coordinate[1]})\n{self.description}"
     
     def place_chest(self):
@@ -22,6 +22,7 @@ for i in range(4):
 house[0][0].description = "Starting position"
 house[3][3].description = "Exit"
 def setup():
+    global house
     num_chest = 5
     available_spot = []
     for i in range(4):
@@ -33,13 +34,13 @@ def setup():
         a = random.randint(0, len(available_spot)-1)
         house[available_spot[a][0]][available_spot[a][1]].place_chest()
         available_spot.pop(a)
-
-for i in range(4):
-    for j in range(4):
-        if (i == 0 and j == 0) or (i == 3 and j == 3):
-            print('+', end=' ')
-        elif house[i][j].chest:
-            print(1, end=' ')
-        elif not house[i][j].chest:
-            print(0, end=' ')
-    print("\n", end='')
+def print_house():
+    for i in range(4):
+        for j in range(4):
+            if (i == 0 and j == 0) or (i == 3 and j == 3):
+                print('+', end=' ')
+            elif house[i][j].chest:
+                print(1, end=' ')
+            elif not house[i][j].chest:
+                print(0, end=' ')
+        print("\n", end='')
