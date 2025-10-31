e2025cdfd0c749b88cbf25ade86d84c54ffd5217
leota0608 <xichen.tao1@rbe.sk.ca>
2025-10-31 14:05:46 -0600

    ...

--- README.md
+++ README.md
@@ -7,7 +7,13 @@ Story line:
     successfully! Remember what cames with treasure is danger...
 </pre>
 This code does the following:
-- Creates a room `class`
+- Creates a tree `class`
 - Creates a 2d `list` containing 4*4 = 16 room classes
-    + Randomly place 5 chests into the rooms
-- Outputs the house `list` using `0` as no chest, `1` as there is a chest and `+` as **entrance** and **exist**
+    + Randomly place 5 chests into the trees
+- Outputs the house `list` using `0` as no chest, `1` as there is a chest and `+` as **entrance** and **exist**  
+- Tree names:
+    1. Oak
+    2. Maple
+    3. Pine
+    4. Birch
+    5. Willow
--- main.py
+++ main.py
@@ -1,46 +1,53 @@
 import random
-class Room:
-    def __init__(self, coordinate, description):
+class Tree:
+    def __init__(self, name, description):
         #attributes
-        self.coordinate = coordinate
+        self.name = name
         self.description = description
         self.chest = False
         
     def __str__(self):
-        return f"This room is at ({self.coordinate[0]}, {self.coordinate[1]})\n{self.description}"
+        return self.name.title()
     
     def place_chest(self):
         self.chest = True
- 
 
-house = []
+
+class Player:
+    def __init__(self, map, x, y):
+        self.map = map
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return f"Player is at position [{self.x}, {self.y}]"
+    
+
+oak = Tree("oak", "Nothing here...")
+pine = Tree("pine", "Nothing here...")
+maple = Tree("maple", "Nothing here...")
+birch = Tree("birch", "Nothing here...")
+willow = Tree("willow", "Nothing here...")
+
+forest = [oak, pine, maple, birch, willow]
+
+map = []
+available_spots = []
 for i in range(4):
-    a = []
+    row = []
     for j in range(4):
-        a.append(Room([i,j], "Nothing here..."))
-    house.append(a)
-house[0][0].description = "Starting position"
-house[3][3].description = "Exit"
-def setup():
-    global house
-    num_chest = 5
-    available_spot = []
-    for i in range(4):
-        for j in range(4):
-            if (i == 0 and j == 0) or (i == 3 and j == 3):
-                continue
-            available_spot.append([i, j])
-    for i in range(num_chest):
-        a = random.randint(0, len(available_spot)-1)
-        house[available_spot[a][0]][available_spot[a][1]].place_chest()
-        available_spot.pop(a)
-def print_house():
-    for i in range(4):
-        for j in range(4):
-            if (i == 0 and j == 0) or (i == 3 and j == 3):
-                print('+', end=' ')
-            elif house[i][j].chest:
-                print(1, end=' ')
-            elif not house[i][j].chest:
-                print(0, end=' ')
-        print("\n", end='')
+        row.append(0)
+        available_spots.append([i, j])
+    map.append(row)
+available_spots.pop(0)
+available_spots.pop(-1)
+player = Player(map, 0, 0)
+
+for i in range(len(forest)):
+    a = random.randint(0, len(available_spots)-1)
+    map[available_spots[a][0]][available_spots[a][1]] = forest[i]
+    available_spots.pop(a)
+
+for i in range(4):
+    for j in range(4):
+        print(map[i][j], end=' ')
+    print("\n", end='')
